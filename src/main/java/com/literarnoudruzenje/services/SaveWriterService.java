db33e58b8e967741f520d22f780ed80741df8a65
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-12-26 19:55:15 +0100

    added necessary services and parameters in the model

---
+++ src/main/java/com/literarnoudruzenje/services/SaveWriterService.java
@@ -0,0 +1,16 @@
+package com.literarnoudruzenje.services;
+
+import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SaveWriterService implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+    }
+}
