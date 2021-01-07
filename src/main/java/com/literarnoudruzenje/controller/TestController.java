33ec28ee0dbc36fc26a39deca4280868db64c767
syn-petarcurcin <petar.curcin1@synechron.com>
2021-01-08 00:22:32 +0100

    upload one pdf file implemented

--- src/main/java/com/literarnoudruzenje/controller/TestController.java
+++ src/main/java/com/literarnoudruzenje/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.literarnoudruzenje.controller;
-
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import com.literarnoudruzenje.dto.FormFieldsDto;
 import com.literarnoudruzenje.dto.FormSubmissionDto;
 import com.literarnoudruzenje.dto.ProcessDto;
+import org.apache.commons.compress.utils.IOUtils;
 import org.camunda.bpm.engine.*;
 import org.camunda.bpm.engine.form.FormField;
 import org.camunda.bpm.engine.form.TaskFormData;
@@ -11,15 +14,23 @@ import org.camunda.bpm.engine.impl.form.validator.FormFieldValidatorException;
 import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.value.FileValue;
 import org.camunda.bpm.model.bpmn.instance.Process;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.xml.xpath.XPath;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.List;
 
@@ -44,7 +55,7 @@ public class TestController {
 
     @GetMapping(path = "/startReader")
     public ProcessDto startReaderProcess(){
-        ProcessInstance pi = runtimeService.startProcessInstanceByKey("ReaderRegistration");
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey("Writer_registration");
         runtimeService.setVariable(pi.getId(), "piId", pi.getId());
         ProcessDto processDto = new ProcessDto();
         processDto.setProcessId(pi.getId());
@@ -66,12 +77,18 @@ public class TestController {
     }
 
     @GetMapping(path = "/activate/{piId}", produces = "application/json")
-    public @ResponseBody String activateUser(@PathVariable String piId) {
+    public @ResponseBody ResponseEntity activateUser(@PathVariable String piId) {
 
-        MessageCorrelationResult ecmMessageArrived = runtimeService.createMessageCorrelation("UserActivation")
-                .processInstanceId(piId)
-                .correlateWithResult();
-        return "";
+        try {
+            MessageCorrelationResult ecmMessageArrived = runtimeService.createMessageCorrelation("UserActivation")
+                    .processInstanceId(piId)
+                    .correlateWithResult();
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("User already activated his account");
+        }
     }
 
 
@@ -84,6 +101,54 @@ public class TestController {
         runtimeService.setVariable(processInstanceId, "registration", dto);
         formService.submitTaskForm(taskId, map);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/uploadFile/{taskId}", produces = "application/json")
+    public @ResponseBody ResponseEntity post(@RequestParam("file") MultipartFile file, @PathVariable String taskId) throws IOException {
+        Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+        String processInstanceId = task.getProcessInstanceId();
+        FileValue typedFileValue = Variables
+                .fileValue(file.getOriginalFilename())
+                .file(file.getBytes())
+                .mimeType("application/pdf")
+                .encoding("UTF-8")
+                .create();
+        runtimeService.setVariable(processInstanceId, "file", typedFileValue);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @RequestMapping(path = "/download/{fileName}", method = RequestMethod.GET)
+    public ResponseEntity<Resource> download(@PathVariable String fileName,@RequestParam("taskId") String taskId) throws IOException {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName + ".pdf");
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        FileValue newFile = runtimeService.getVariableTyped(taskId,"file");
+        InputStream is = newFile.getValue();
+        File targetFile = new File(newFile.getFilename());
+
+        java.nio.file.Files.copy(
+                is,
+                targetFile.toPath(),
+                StandardCopyOption.REPLACE_EXISTING);
+
+        byte[] bytes = FileUtils.readFileToByteArray(targetFile);
+
+
+        IOUtils.closeQuietly(is);
+
+        ByteArrayResource resource = new ByteArrayResource(bytes);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(targetFile.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
     private HashMap<String, Object> mapListToDto(List<FormSubmissionDto> list)
