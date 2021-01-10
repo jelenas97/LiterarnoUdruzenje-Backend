e852c3cfcb98b4ea841f42945f0906dd8567d2f8
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2021-01-10 13:17:23 +0100

    added UserDTO and UserTokenState

---
+++ src/main/java/com/literarnoudruzenje/dto/UserDTO.java
@@ -0,0 +1,33 @@
+package com.literarnoudruzenje.dto;
+
+
+import com.literarnoudruzenje.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private List<String> roles;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+    }
+
+}
