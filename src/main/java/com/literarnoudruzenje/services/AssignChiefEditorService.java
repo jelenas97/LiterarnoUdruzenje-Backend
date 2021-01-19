19debd6b7e281e3157759b0553b76d06e84adc04
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2021-01-19 22:32:16 +0100

    Update AssignChiefEditorService.java

--- src/main/java/com/literarnoudruzenje/services/AssignChiefEditorService.java
+++ src/main/java/com/literarnoudruzenje/services/AssignChiefEditorService.java
@@ -1,12 +1,33 @@
 package com.literarnoudruzenje.services;
 
+import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.identity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class AssignChiefEditorService implements JavaDelegate {
+
+    @Autowired
+    IdentityService identityService;
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
 
-    }
+            List<User> userList = identityService.createUserQuery().memberOfGroup("editors").list();
+            List<String> userIds = new ArrayList<>();
+            for(User u : userList) {
+                userIds.add(u.getId());
+                System.out.println(u.getId());
+            }
+
+            if(!userList.isEmpty()) {
+                delegateExecution.setVariable("chiefEditor", userIds.get(0));
+            }
+        }
 }
