93134623bfd897b8f3df968a32a42cd32554d21c
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2021-01-19 22:27:05 +0100

    added book model

---
+++ src/main/java/com/literarnoudruzenje/model/Book.java
@@ -0,0 +1,27 @@
+package com.literarnoudruzenje.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String synopsis;
+
+    @ManyToMany
+    @JoinTable(name = "book_genres",
+            joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id", referencedColumnName = "id"))
+    private List<Genre> genres;
+}
