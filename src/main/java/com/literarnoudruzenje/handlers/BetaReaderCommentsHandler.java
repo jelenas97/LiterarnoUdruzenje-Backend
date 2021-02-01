8c19a9bbacd66784cfe72d5b648635d418e7f36f
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2021-02-01 18:14:56 +0100

    Update BetaReaderCommentsHandler.java

--- src/main/java/com/literarnoudruzenje/handlers/BetaReaderCommentsHandler.java
+++ src/main/java/com/literarnoudruzenje/handlers/BetaReaderCommentsHandler.java
@@ -1,12 +1,16 @@
 package com.literarnoudruzenje.handlers;
 
+import com.literarnoudruzenje.dto.FormSubmissionDto;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
+
+import java.util.List;
 
 public class BetaReaderCommentsHandler implements TaskListener {
 
     @Override
     public void notify(DelegateTask delegateTask) {
-
+        List<FormSubmissionDto> dto = (List<FormSubmissionDto>) delegateTask.getVariable("registration");
+        delegateTask.setVariable( "betaReaderCommentForm", dto);
     }
 }
