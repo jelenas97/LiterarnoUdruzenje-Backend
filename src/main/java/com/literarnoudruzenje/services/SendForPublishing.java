2d57e962cc1fb282506053baa72970a2df91ea61
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2021-02-02 22:05:22 +0100

    Create SendForPublishing.java

---
+++ src/main/java/com/literarnoudruzenje/services/SendForPublishing.java
@@ -0,0 +1,19 @@
+package com.literarnoudruzenje.services;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SendForPublishing implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        delegateExecution.getVariable( "files");
+
+
+
+    }
+}
